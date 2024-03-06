2716b0a181816548c869561d1adb871df95f51ff
jra <julien.gault.50@gmail.com>
2024-03-06 15:26:21 +0100

    Add project files.

---
+++ MSLearn.CSharpConvertCast.Challenge2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
