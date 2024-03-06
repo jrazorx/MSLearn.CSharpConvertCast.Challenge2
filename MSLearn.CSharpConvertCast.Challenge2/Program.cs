d7361f59fb3fc1378df46e2273a71f00d2d97384
jra <julien.gault.50@gmail.com>
2024-03-06 15:35:08 +0100

    solution

--- MSLearn.CSharpConvertCast.Challenge2/Program.cs
+++ MSLearn.CSharpConvertCast.Challenge2/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-int value1 = 12;
+int     value1 = 12;
 decimal value2 = 6.2m;
-float value3 = 4.3f;
+float   value3 = 4.3f;
 
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
+// Solve for result1: Divide value1 by value2, display the result as an int
+int result1 = Convert.ToInt32((decimal)value1 / value2);
 Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-// Your code here to set result2
+// Solve for result2: Divide value2 by value3, display the result as a decimal
+decimal result2 = value2 / (decimal)value3;
 Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-// Your code here to set result3
+// Solve for result3: Divide value3 by value1, display the result as a float
+float result3 = value3 / value1;
 Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
